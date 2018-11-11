a464422aa9d169b97648c73a15fdf703018d9931
Eric B <burn4live@gmail.com>
2018-11-11 13:49:50 -0700

    finished product

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,15 @@
+function autocomplete(input, latInput, lngInput) {
+  if(!input) return; // skip the function
+  const dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+  // if someone hit enter on address dont submit storeForm
+  input.on('keydown', (e) => {
+    if(e.keycode === 13) e.preventDefault();
+  })
+}
+
+export default autocomplete;
